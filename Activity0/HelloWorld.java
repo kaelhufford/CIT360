c88ca6b1901bc6fbe02f39407a3da90a0cbed61a
Kael Hufford <accentrager@gmail.com>
2026-08-20 11:17:00 -0400

    Init CIT360 8/20/26
    Start of Python -> Java basics

---
+++ Activity0/HelloWorld.java
@@ -0,0 +1,14 @@
+package Activity0;
+
+public class HelloWorld {
+    // Method stub
+    //*
+    // public - acces spec
+    // static - 
+    // void - return type
+    // main - method name
+    // args - ...args.   */
+    public static void main(String[] args){
+        System.out.println("Hello, world.");
+    }
+}
