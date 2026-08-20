985fff74c89c6c422ff2595fbbad799216315842
Kael Hufford <accentrager@gmail.com>
2026-08-20 11:20:44 -0400

    Removed HelloWorld.java

--- Activity0/Activity0.java
+++ Activity0/Activity0.java
@@ -1,6 +1,13 @@
 package Activity0;
 
 public class Activity0 {
+    /**
+     * public - access spec
+     * static - def of function
+     * void - return type
+     * main - method name
+     * @param args - ...args...
+     */
     public static void main(String[] args) {
         int[] testArray= {1,1,3,4,2,6,8,2,5,2,1,12,8,1819};
         ArrayOperations.removeDuplicatesFromArray(testArray);
