de3e0a38c1f988c8fc81166dabf7a81aa2caaf71
Kael Hufford <accentrager@gmail.com>
2026-08-25 11:59:26 -0400

    Fixed areArraysEqual method

--- Activity0/ArrayOperations.java
+++ Activity0/ArrayOperations.java
@@ -2,6 +2,8 @@ package Activity0;
 
 import java.util.Random;
 
+import javax.swing.plaf.TreeUI;
+
 public class ArrayOperations {
     /** printArray - Takes in an array to print it index by index.
      */
@@ -30,11 +32,18 @@ public class ArrayOperations {
     /**areArraysEqual - Returns T/F if the two given arrays are index for index equal
      */
     public static boolean areArraysEqual(int[] arrayOne, int[] arrayTwo){
-        if (arrayOne == arrayTwo){
-            return true;
-        } else {
+
+        boolean equal = true;
+
+        try {
+            for (int i = 0; i < arrayOne.length; i++){
+                if (arrayOne[i] != arrayTwo[i]){ equal = false; break; }
+            }
+            return equal;
+        } catch (ArrayIndexOutOfBoundsException e) {
             return false;
         }
+
     }
 
     /** generateIntArray - Returns a randomly generated array based off of the length
@@ -43,7 +52,7 @@ public class ArrayOperations {
         int[] randomArray = new int[length];
         Random random = new Random();
         for (int i = 0; i < length; i++){
-            randomArray[i] = random.nextInt(max);
+            randomArray[i] = Math.round(random.);
         }   
         return randomArray;
     }
